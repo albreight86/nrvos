f49d482c93f59365628bb29733036451f7dd476e
A Breight <albreight86@gmail.com>
2017-03-01 22:19:43 -0700

    sovrn commit one

---
+++ src/main/java/com/arb/SpringMain.java
@@ -0,0 +1,24 @@
+/*
+ * COPYRIGHT 2017.  Alan R Breight
+ */
+ 
+package com.arb;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringMain
+{
+
+   public static void main (String[] args)
+   {
+      new SpringMain();
+   }
+
+   public SpringMain()
+   {
+      // open/read the application context file
+      ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+   }
+
+}
